e8735c116c50abe61f324b33dfb8f70882382514
Reeyyyh <rhnlnv@gmail.com>
2025-02-07 02:20:45 +0700

    testing sending email

--- app/Http/Controllers/sendEmail.php
+++ app/Http/Controllers/sendEmail.php
@@ -5,11 +5,20 @@ namespace App\Http\Controllers;
 use App\Mail\emailSendingStructura;
 use Illuminate\Support\Facades\Mail;
 
+// funsi ini di buat untuk uji coba
 class sendEmail extends Controller
 {
-    public function index()
+    // public function SendEmailFunc()
+    // {
+    //     // Mail::to($request->user())->send(new OrderShipped($order));
+    //     Mail::to('email@gmail.com')->send(new emailSendingStructura());
+    // }
+
+    public function SendEmailFunc()
     {
-        // Mail::to($request->user())->send(new OrderShipped($order));
-        Mail::to('email@gmail.com')->send(new emailSendingStructura());
+        // Kirim email uji coba ke alamat tertentu
+        // Mail::to('email@gmail.com')->send(new emailSendingStructura());
+
+        return response()->json(['message' => 'Email berhasil dikirim!']);
     }
 }
