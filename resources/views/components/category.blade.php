9c580d7c27abb0d5bf2bace5abf516b734e7eb86
github.marifirfan <github.marifirfan@gmail.com>
2025-02-07 16:39:42 +0700

    add category component

---
+++ resources/views/components/category.blade.php
@@ -0,0 +1,124 @@
+<div class="bg-smoothcream py-4 px-4 mt-8 mx-auto w-full md:w-[1280px] rounded-[10px]">
+    <div class="max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        <!-- Card 1 -->
+        <div class="bg-[#1E375A] text-white rounded-lg flex items-center h-20 w-[240px]">
+            <img src="{{ asset('images/landingpages/category/paint-colour-tins.jpg') }}" alt="Material Konstruksi"
+                class="mr-3 w-1/3 h-full rounded-lg">
+            <div>
+                <h3 class="font-semibold text-[12px] w-1/2">Material Konstruksi</h3>
+                <div class="leading-none">
+                    <p class="text-sm opacity-80 text-[8px] ">Material dasar untuk </p>
+                    <p class="text-sm opacity-80 text-[8px] -mt-2">pondasi, dinding, dan plafon.</p>
+                </div>
+            </div>
+        </div>
+        <!-- Card 2 -->
+        <div class="bg-[#1E375A] text-white rounded-lg flex items-center h-20 w-[240px]">
+            <img src="{{ asset('images/landingpages/category/paint-colour-tins.jpg') }}" alt="Material Konstruksi"
+                class="mr-3 w-1/3 h-full rounded-lg">
+            <div>
+                <h3 class="font-semibold text-[12px] w-1/2">Besi dan Baja</h3>
+                <div class="leading-none">
+                    <p class="text-sm opacity-80 text-[8px] ">Bahan kuat untuk rangka dan </p>
+                    <p class="text-sm opacity-80 text-[8px] -mt-2">struktur bangunan.</p>
+                </div>
+            </div>
+        </div>
+        <!-- Card 3 -->
+        <div class="bg-[#1E375A] text-white rounded-lg flex items-center h-20 w-[240px]">
+            <img src="{{ asset('images/landingpages/category/paint-colour-tins.jpg') }}" alt="Material Konstruksi"
+                class="mr-3 w-1/3 h-full rounded-lg">
+            <div>
+                <h3 class="font-semibold text-[12px] w-1/2">Atap & Rangka</h3>
+                <div class="leading-none">
+                    <p class="text-sm opacity-80 text-[8px] ">Material pelindung dan </p>
+                    <p class="text-sm opacity-80 text-[8px] -mt-2">kerangka atap.</p>
+                </div>
+            </div>
+        </div>
+        <!-- Card 4 -->
+        <div class="bg-[#1E375A] text-white rounded-lg flex items-center h-20 w-[240px]">
+            <img src="{{ asset('images/landingpages/category/paint-colour-tins.jpg') }}" alt="Material Konstruksi"
+                class="mr-3 w-1/3 h-full rounded-lg">
+            <div>
+                <h3 class="font-semibold text-[12px] w-1/2">Kayu & Plywood</h3>
+                <div class="leading-none">
+                    <p class="text-sm opacity-80 text-[8px] ">Bahan serbaguna untuk </p>
+                    <p class="text-sm opacity-80 text-[8px] -mt-2">interior dan kerangka.</p>
+                </div>
+            </div>
+        </div>
+        <!-- Card 5 -->
+        <div class="bg-[#1E375A] text-white rounded-lg flex items-center h-20 w-[240px]">
+            <img src="{{ asset('images/landingpages/category/paint-colour-tins.jpg') }}" alt="Material Konstruksi"
+                class="mr-3 w-1/3 h-full rounded-lg">
+            <div>
+                <h3 class="font-semibold text-[12px] w-1/2">Cat & Pelapis</h3>
+                <div class="leading-none">
+                    <p class="text-sm opacity-80 text-[8px] ">Pelapis untuk estetika dan </p>
+                    <p class="text-sm opacity-80 text-[8px] -mt-2">perlindungan./p>
+                </div>
+            </div>
+        </div>
+        <!-- Card 1 -->
+        <div class="bg-[#1E375A] text-white rounded-lg flex items-center h-20 w-[240px]">
+            <img src="{{ asset('images/landingpages/category/paint-colour-tins.jpg') }}" alt="Material Konstruksi"
+                class="mr-3 w-1/3 h-full rounded-lg">
+            <div>
+                <h3 class="font-semibold text-[12px] w-1/2">Material Konstruksi</h3>
+                <div class="leading-none">
+                    <p class="text-sm opacity-80 text-[8px] ">Material dasar untuk </p>
+                    <p class="text-sm opacity-80 text-[8px] -mt-2">pondasi, dinding, dan plafon.</p>
+                </div>
+            </div>
+        </div>
+        <!-- Card 2 -->
+        <div class="bg-[#1E375A] text-white rounded-lg flex items-center h-20 w-[240px]">
+            <img src="{{ asset('images/landingpages/category/paint-colour-tins.jpg') }}" alt="Material Konstruksi"
+                class="mr-3 w-1/3 h-full rounded-lg">
+            <div>
+                <h3 class="font-semibold text-[12px] w-1/2">Besi dan Baja</h3>
+                <div class="leading-none">
+                    <p class="text-sm opacity-80 text-[8px] ">Bahan kuat untuk rangka dan </p>
+                    <p class="text-sm opacity-80 text-[8px] -mt-2">struktur bangunan.</p>
+                </div>
+            </div>
+        </div>
+        <!-- Card 3 -->
+        <div class="bg-[#1E375A] text-white rounded-lg flex items-center h-20 w-[240px]">
+            <img src="{{ asset('images/landingpages/category/paint-colour-tins.jpg') }}" alt="Material Konstruksi"
+                class="mr-3 w-1/3 h-full rounded-lg">
+            <div>
+                <h3 class="font-semibold text-[12px] w-1/2">Atap & Rangka</h3>
+                <div class="leading-none">
+                    <p class="text-sm opacity-80 text-[8px] ">Material pelindung dan </p>
+                    <p class="text-sm opacity-80 text-[8px] -mt-2">kerangka atap.</p>
+                </div>
+            </div>
+        </div>
+        <!-- Card 4 -->
+        <div class="bg-[#1E375A] text-white rounded-lg flex items-center h-20 w-[240px]">
+            <img src="{{ asset('images/landingpages/category/paint-colour-tins.jpg') }}" alt="Material Konstruksi"
+                class="mr-3 w-1/3 h-full rounded-lg">
+            <div>
+                <h3 class="font-semibold text-[12px] w-1/2">Kayu & Plywood</h3>
+                <div class="leading-none">
+                    <p class="text-sm opacity-80 text-[8px] ">Bahan serbaguna untuk </p>
+                    <p class="text-sm opacity-80 text-[8px] -mt-2">interior dan kerangka.</p>
+                </div>
+            </div>
+        </div>
+        <!-- Card 5 -->
+        <div class="bg-[#1E375A] text-white rounded-lg flex items-center h-20 w-[240px]">
+            <img src="{{ asset('images/landingpages/category/paint-colour-tins.jpg') }}" alt="Material Konstruksi"
+                class="mr-3 w-1/3 h-full rounded-lg">
+            <div>
+                <h3 class="font-semibold text-[12px] w-1/2">Cat & Pelapis</h3>
+                <div class="leading-none">
+                    <p class="text-sm opacity-80 text-[8px] ">Pelapis untuk estetika dan </p>
+                    <p class="text-sm opacity-80 text-[8px] -mt-2">perlindungan.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
