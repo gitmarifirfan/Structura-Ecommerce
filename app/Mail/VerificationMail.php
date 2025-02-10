48cd68fe0a0e4b016edf016dd134abfb3cb0939a
Reeyyyh <rhnlnv@gmail.com>
2025-02-10 19:07:54 +0700

    make email to verification account

---
+++ app/Mail/VerificationMail.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Mail\Mailables\Content;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class VerificationMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+    public $verificationUrl;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($user, $verificationUrl)
+    {
+        $this->user = $user;
+        $this->verificationUrl = $verificationUrl;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            // use domain email from database
+
+            from: new Address(config('mail.from.address'), config('mail.from.name')),
+            // from: new Address('no-reply@demomailtrap.com', 'Structura'),
+            subject: 'Email Verification - Structura',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.verification',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
