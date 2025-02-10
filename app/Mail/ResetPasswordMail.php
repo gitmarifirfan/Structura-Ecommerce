ef1f652b3bd2900af5fde404f90c992848673125
Reeyyyh <rhnlnv@gmail.com>
2025-02-10 19:07:20 +0700

    make email for reset password

---
+++ app/Mail/ResetPasswordMail.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Mail;
+
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ResetPasswordMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $resetUrl;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($resetUrl)
+    {
+        $this->resetUrl = $resetUrl;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            // use domain email from database
+
+            from: new Address(config('mail.from.address'), config('mail.from.name')),
+            // from: new Address('no-reply@demomailtrap.com', 'Structura'),
+            subject: 'Reset Password Email - Structura',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.resetPassword',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
